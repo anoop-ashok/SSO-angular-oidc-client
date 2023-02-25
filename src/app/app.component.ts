0f8df45fa03e44fad21c4d17433736020c3b837f
Anoop A <a.anoop@tcs.com>
2023-02-25 18:03:18 +0100

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { NavigationEnd, Router } from '@angular/router';
+import { OidcSecurityService } from 'angular-auth-oidc-client';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  isAuthenticated: boolean = false;
+  constructor(private http: HttpClient,
+    private router: Router,
+    public oidcSecurityService: OidcSecurityService) {}
+  ngOnInit(): void {
+    throw new Error('Method not implemented.');
+  }
   title = 'SSO-angular-oidc-client';
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,27 @@
+import { OidcSecurityService } from 'angular-auth-oidc-client';
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    RouterModule.forRoot(
+      [
+        { path: "", component: DashboardComponent}
+      ]
+    )
   ],
-  providers: [],
+  providers: [OidcSecurityService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
